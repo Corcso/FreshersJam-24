6ebde61bec3709a77d604adad4f7b829be316d00
ErythiaSea <2201593@uad.ac.uk>
2024-09-28 15:30:42 +0100

    Edge Detection

--- Scripts/enemyBasic.cs
+++ Scripts/enemyBasic.cs
@@ -1,44 +1,53 @@
 using Godot;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Drawing;
 
 public partial class enemyBasic : CharacterBody2D
 {
     [Export]
-    public const float moveVelocity = 40.0f;
+    public bool moveLeft = false;
     [Export]
-    public bool moveLeft = true;
+    public const float moveVelocity = 400.0f;
+    [Export]
+    public bool edgeDetect = true;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-		//Vector2 center = Position;
 
 		// Add the gravity.
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
 		else
 		{
-			// Prepare to raycast
-			var spaceState = GetWorld2D().DirectSpaceState;
-			var query = PhysicsRayQueryParameters2D.Create(GlobalPosition, new Vector2(GlobalPosition.X + (moveLeft ? 1 : -1), GlobalPosition.Y - 1));
-			query.Exclude = new Godot.Collections.Array<Rid> { GetRid() };
-			var result = spaceState.IntersectRay(query);
-
-			// Flip moving direction if raycast fails
-			if (result.Values.Count == 0)
+			if (edgeDetect)
 			{
-				moveLeft = !moveLeft;
-				Debug.Print("flipping");
+                // Check the "foot" approaching the edge of the platform, flip if there's no platform
+                if (moveLeft)
+				{
+					RayCast2D lfoot = GetNode<RayCast2D>("leftFoot");
+					if (!lfoot.IsColliding()) moveLeft = !moveLeft;
+				}
+				else
+				{
+					RayCast2D rfoot = GetNode<RayCast2D>("rightFoot");
+					if (!rfoot.IsColliding()) moveLeft = !moveLeft;
+				}
 			}
 		}
-        velocity.X = (moveLeft ? 1 : -1) * moveVelocity;
-
-		Velocity = velocity;
+		// Move enemy
+		velocity.X = (moveLeft ? -1 : 1) * moveVelocity;
+        Velocity = velocity;
 		MoveAndSlide();
+		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		{
+			GetSlideCollision(i);
+			Debug.Print(GetSlideCollisionCount().ToString());
+		}
 	}
 }
