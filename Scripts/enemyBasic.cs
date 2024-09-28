5075c9e295f0e28c77bc4d5993833c86a43bf3ae
ErythiaSea <2201593@uad.ac.uk>
2024-09-28 16:15:46 +0100

    Collision detection "improvements"

--- Scripts/enemyBasic.cs
+++ Scripts/enemyBasic.cs
@@ -1,53 +1,78 @@
 using Godot;
+using Microsoft.VisualBasic.FileIO;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Drawing;
 
-public partial class enemyBasic : CharacterBody2D
+public partial class enemyBasic : RigidBody2D
 {
     [Export]
     public bool moveLeft = false;
     [Export]
-    public const float moveVelocity = 400.0f;
+    public const float moveVelocity = 100.0f;
     [Export]
     public bool edgeDetect = true;
 
     // Get the gravity from the project settings to be synced with RigidBody nodes.
     public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    public override void _Ready()
+    {
+        BodyEntered += (Node body) => Collision(body);
+    }
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
-		else
+    public override void _IntegrateForces(PhysicsDirectBodyState2D state)
+	{
+		Vector2 velocity = state.LinearVelocity;
+		state.AngularVelocity = 0;
+        // Add the gravity.
+        //if (!IsOnFloor())
+        //	velocity.Y += gravity * (float)delta;
+        //else
+
+        RayCast2D lfoot = GetNode<RayCast2D>("leftFoot");
+        RayCast2D rfoot = GetNode<RayCast2D>("rightFoot");
+        //{
+        if (edgeDetect)
 		{
-			if (edgeDetect)
+            // Check the "foot" approaching the edge of the platform, flip if there's no platform
+            
+            if (moveLeft)
 			{
-                // Check the "foot" approaching the edge of the platform, flip if there's no platform
-                if (moveLeft)
-				{
-					RayCast2D lfoot = GetNode<RayCast2D>("leftFoot");
-					if (!lfoot.IsColliding()) moveLeft = !moveLeft;
-				}
-				else
-				{
-					RayCast2D rfoot = GetNode<RayCast2D>("rightFoot");
-					if (!rfoot.IsColliding()) moveLeft = !moveLeft;
-				}
+				
+				if (!lfoot.IsColliding() && rfoot.IsColliding()) moveLeft = !moveLeft;
 			}
+			else
+			{
+				
+				if (!rfoot.IsColliding() && lfoot.IsColliding()) moveLeft = !moveLeft;
+			}
+				
 		}
-		// Move enemy
-		velocity.X = (moveLeft ? -1 : 1) * moveVelocity;
-        Velocity = velocity;
-		MoveAndSlide();
-		for (int i = 0; i < GetSlideCollisionCount(); i++)
-		{
-			GetSlideCollision(i);
-			Debug.Print(GetSlideCollisionCount().ToString());
-		}
+		if(rfoot.IsColliding() || lfoot.IsColliding()) velocity.X = (moveLeft ? -1 : 1) * moveVelocity;
+        //}
+        // Move enemy
+
+        state.LinearVelocity = velocity;
+		//MoveAndSlide();
+		//for (int i = 0; i < GetSlideCollisionCount(); i++)
+		//{
+		//	GetSlideCollision(i);
+		//	Debug.Print(GetSlideCollisionCount().ToString());
+		//}
+
 	}
+
+    private void Collision(Node body) {
+        Debug.Print("collision with " + body.Name.ToString());
+        player possiblePlayer = body.GetParent().GetNodeOrNull<player>("./" + body.Name);
+        if (possiblePlayer != null)
+        {
+            if (possiblePlayer.GlobalPosition.Y > GlobalPosition.Y && possiblePlayer.Velocity.Y < LinearVelocity.Y)
+            {
+                Debug.Print("enemy should die");
+            }
+        }
+    }
 }
